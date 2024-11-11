b9a79870c965dc8e1a019a7621a612767ad25d6b
Wakabambo <Bakks7017@gmail.com>
2024-11-11 14:09:27 +0300

    Create FLS.lua

---
+++ FLS.lua
@@ -0,0 +1,257 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("FLS by @Wakabambo", "RJTheme3")
+
+local Tab = Window:NewTab("1")
+
+local Section = Tab:NewSection("Mega Lift")
+
+Section:NewButton("Pencil", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Pencil.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Stick", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Stick.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Mouse", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Mouse.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Water", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Water.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Bottle", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Bottle.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Texbook", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.texbook.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Bucket", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Bucket.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Wood", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Wood.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Guitar", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.guitar.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Dumbbell", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Dumbbell.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Chair", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Chair.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Cart", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Cart.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("TV", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.TV.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Bicycle", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Bicycle.Script.RemoteEvent:FireServer()
+end
+end)Section:NewButton("Desk", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.desk.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Bed", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Deb.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Log", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Log.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Canoe", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Canoe.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Tyre", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.tyre.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Drum", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Drum.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Hydrant", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Hydrant.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Piano", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Piano.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Motorcycle", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Motorcycle.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Flag", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Flag.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("ATM", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.ATM.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("EVO", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.EVO.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Van", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Van.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Tree", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Tree.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Container", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Container.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Bus", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Bus.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Truck", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Truck.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Windmill", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Windmill.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Jet", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Jet.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Tank", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Tank.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Battleship", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Battlesgip.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Speaker", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Speaker.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Building", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Building.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Rocket", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Rocket.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Castle", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Castle.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Torpedo", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Torpedo.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("UFO", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.UFO.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Pyramid", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Pyramid.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Mountain", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Mountain.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Volcano", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Volcano.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Meteorite", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Meteorite.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Moon", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.moon.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Mercury", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Mercury.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Mars", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Mars.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Saturn", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Saturn.Script.RemoteEvent:FireServer()
+end
+end)
+Section:NewButton("Jupiter", "ButtonInfo", function()
+    for i = 1, 5000 do
+game:GetService("Players").LocalPlayer.Character.Jupiter.Script.RemoteEvent:FireServer()
+end
+end)
